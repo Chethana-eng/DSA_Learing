2ac1035085f3e24edf6aa53d5091406c2cbb0adf
Chethana.K <chethanakoya@gmail.com>
2025-08-17 14:45:22 +0530

    ReverseString- (recursion + 2 pointer solution)

--- reverseString.java
+++ reverseString.java
@@ -30,3 +30,15 @@ class reverseString{
         }
     }
 }
+
+
+
+        // int left =0;
+        // int right = s.length -1;
+        // while(left<right){
+        //     char temp = s[left];
+        //     s[left] = s[right];
+        //     s[right] = temp;
+        //     left++;
+        //     right--;
+        // }
